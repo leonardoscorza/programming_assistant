16e4acad124e9a4ce983ba84b7521b7dbe33da00
Leonardo Scorza <leonardoscorzas88@gmail.com>
2016-09-13 14:50:22 -0300

    db

---
+++ app/controllers/bot_controller.rb
@@ -0,0 +1,5 @@
+class BotController < ApplicationController
+  def facebook
+    render text: 'abcd abcd'
+  end
+end
---
+++ test/controllers/bot_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BotControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
