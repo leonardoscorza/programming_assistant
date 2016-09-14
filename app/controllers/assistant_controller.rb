08377673edf617a784ea37b3c6144ecb0f18b76c
Leonardo Scorza <leonardoscorzas88@gmail.com>
2016-09-14 13:39:05 -0300

    Programming assistant v1

--- app/controllers/assistant_controller.rb
+++ app/controllers/assistant_controller.rb
@@ -4,7 +4,7 @@ class AssistantController < ApplicationController
     page = WikipediaConsulter.find_by_name(query)
 
     if page.text.present?
-      text = "#{page.text.slice(0, 120)}... \b link completo: #{page.fullurl}"
+      text = "#{page.text.slice(0, 150)}... \b link completo: #{page.fullurl}"
     else
       text = "Não encontrei essa página na Wikipedia em inglês :("
     end
