2d8bc421a8a87643a5bcf3029d204983982d9afd
Leonardo Scorza <leonardoscorzas88@gmail.com>
2016-09-14 00:44:09 -0300

    Wikipedia

--- app/controllers/assistant_controller.rb
+++ app/controllers/assistant_controller.rb
@@ -3,7 +3,7 @@ class AssistantController < ApplicationController
     query = (params[:result][:parameters]['any'].present?)? params[:result][:parameters]['any'] : "Ruby On Rails"
     page = WikipediaConsulter.find_by_name(query)
 
-    text = (page.text.present?)? page.text.slice(0, 60) : ''
+    text = (page.text.present?)? page.text.slice(0, 120) : ''
     response =
     {
       "speech": "#{text}... \b link completo: #{page.fullurl}",
